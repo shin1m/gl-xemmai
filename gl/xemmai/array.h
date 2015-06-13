bd4c46a4ac7b7d44327f62acbad4244a9014d57a
Shin-ichi MORITA <shin1morita@gmail.com>
2015-06-13 12:52:08 +0900

    Updated the required xemmai version.

--- gl/xemmai/array.h
+++ gl/xemmai/array.h
@@ -81,8 +81,8 @@ struct t_type_of<t_array_of<T>> : t_type
 	virtual void f_scan(t_object* a_this, t_scan a_scan);
 	virtual void f_finalize(t_object* a_this);
 	virtual t_scoped f_construct(t_object* a_class, t_scoped* a_stack, size_t a_n);
-	virtual void f_get_at(t_object* a_this, t_scoped* a_stack);
-	virtual void f_set_at(t_object* a_this, t_scoped* a_stack);
+	virtual size_t f_get_at(t_object* a_this, t_scoped* a_stack);
+	virtual size_t f_set_at(t_object* a_this, t_scoped* a_stack);
 };
 
 template<typename T>
@@ -123,27 +123,29 @@ t_scoped t_type_of<t_array_of<T>>::f_construct(t_object* a_class, t_scoped* a_st
 }
 
 template<typename T>
-void t_type_of<t_array_of<T>>::f_get_at(t_object* a_this, t_scoped* a_stack)
+size_t t_type_of<t_array_of<T>>::f_get_at(t_object* a_this, t_scoped* a_stack)
 {
 	t_native_context context;
 	f_check<t_array_of<T>>(a_this, L"this");
-	t_scoped a0 = std::move(a_stack[1]);
+	t_scoped a0 = std::move(a_stack[2]);
 	f_check<size_t>(a0, L"index");
 	a_stack[0].f_construct(f_as<const t_array_of<T>&>(a_this).f_get_at(f_as<size_t>(a0)));
 	context.f_done();
+	return -1;
 }
 
 template<typename T>
-void t_type_of<t_array_of<T>>::f_set_at(t_object* a_this, t_scoped* a_stack)
+size_t t_type_of<t_array_of<T>>::f_set_at(t_object* a_this, t_scoped* a_stack)
 {
 	t_native_context context;
 	f_check<t_array_of<T>>(a_this, L"this");
-	t_scoped a0 = std::move(a_stack[1]);
-	t_scoped a1 = std::move(a_stack[2]);
+	t_scoped a0 = std::move(a_stack[2]);
+	t_scoped a1 = std::move(a_stack[3]);
 	f_check<size_t>(a0, L"index");
 	f_check<T>(a1, L"value");
 	a_stack[0].f_construct(f_as<t_array_of<T>&>(a_this).f_set_at(f_as<size_t>(a0), f_as<T>(a1)));
 	context.f_done();
+	return -1;
 }
 
 }
