343a41245fd094ac6b21f0f491dc75cc70472e48
Shin-ichi MORITA <shin1morita@gmail.com>
2016-03-29 07:45:03 +0900

    Removed tracking native contexts.

--- gl/xemmai/array.h
+++ gl/xemmai/array.h
@@ -125,26 +125,20 @@ t_scoped t_type_of<t_array_of<T>>::f_construct(t_object* a_class, t_scoped* a_st
 template<typename T>
 size_t t_type_of<t_array_of<T>>::f_get_at(t_object* a_this, t_scoped* a_stack)
 {
-	t_native_context context;
-	f_check<t_array_of<T>>(a_this, L"this");
 	t_scoped a0 = std::move(a_stack[2]);
 	f_check<size_t>(a0, L"index");
 	a_stack[0].f_construct(f_as<const t_array_of<T>&>(a_this).f_get_at(f_as<size_t>(a0)));
-	context.f_done();
 	return -1;
 }
 
 template<typename T>
 size_t t_type_of<t_array_of<T>>::f_set_at(t_object* a_this, t_scoped* a_stack)
 {
-	t_native_context context;
-	f_check<t_array_of<T>>(a_this, L"this");
 	t_scoped a0 = std::move(a_stack[2]);
 	t_scoped a1 = std::move(a_stack[3]);
 	f_check<size_t>(a0, L"index");
 	f_check<T>(a1, L"value");
 	a_stack[0].f_construct(f_as<t_array_of<T>&>(a_this).f_set_at(f_as<size_t>(a0), f_as<T>(a1)));
-	context.f_done();
 	return -1;
 }
 
